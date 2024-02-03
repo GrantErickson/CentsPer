d9ef0721807e5ca5ce3f64281b2ebce9252f6c45
Grant Erickson <grant@granterickson.com>
2024-02-03 13:58:16 -0800

    Layout in columns

--- components/CentsPerMileSingle.vue
+++ components/CentsPerMileSingle.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card elevation="3" color="brown" variant="tonal">
+  <v-card elevation="3">
     <v-card-title>
       <div class="text-h4">CentsPer Mile</div>
     </v-card-title>
@@ -8,25 +8,51 @@
       <div>200,000 miles is used as a car's lifetime</div>
     </v-card-subtitle>
     <v-card-text>
-      <v-text-field label="Make" v-model="car.make"></v-text-field>
-      <v-text-field label="Model" v-model="car.model"></v-text-field>
-      <v-text-field label="Year" v-model="car.year"></v-text-field>
-      <v-text-field label="Hotness" v-model="car.hotness"></v-text-field>
-      <v-text-field label="Price" v-model="car.price"></v-text-field>
-      <v-text-field label="Miles" v-model="car.miles"></v-text-field>
-      <v-text-field label="Location" v-model="car.location"></v-text-field>
-      <v-text-field
-        label="Cents per Mile"
-        :model-value="car.centsPerMile()"
-        readonly
-      >
-      </v-text-field>
+      <v-row>
+        <v-col>
+          <v-text-field label="Make" v-model="car.make"></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field label="Model" v-model="car.model"></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field label="Year" v-model="car.year"></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field label="Location" v-model="car.location"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field label="Price" v-model="car.price"></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field label="Miles" v-model="car.miles"></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field
+            label="Hotness"
+            v-model="car.hotness"
+            type="number"
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field
+            label="Cents per Mile"
+            :model-value="car.centsPerMile()"
+            readonly
+          >
+          </v-text-field>
+        </v-col>
+      </v-row>
+
       <v-btn @click="save" color="blue">Save</v-btn>
 
       <v-list v-for="(car, index) in cars" :key="car.key">
         <v-list-item @click="load(index)" elevation="4">
-          {{ car.year }} {{ car.make }} {{ car.model }} at {{ car.location }} with {{ car.hotness }}% hotness =
-          {{ car.centsPerMile() }}¢ per mile 
+          {{ car.year }} {{ car.make }} {{ car.model }} at
+          {{ car.location }} with {{ car.hotness }}% hotness =
+          {{ car.centsPerMile() }}¢ per mile
           <v-icon @click="remove(index)" color="red" icon="mdi-delete">
           </v-icon>
         </v-list-item>
